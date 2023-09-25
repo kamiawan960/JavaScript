309d0f9a5572976f027bbdf4206a2f20c2793fa1
kamiawan960 <kamiawan960@gamil.com>
2023-09-25 17:18:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Brand,CTA,Navbar} from './components'
+import {Blog,Features,Footer,Header,Possibility,WhatGPT3} from './containers'
+import './App.css';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Possibility = () => {
+  return (
+    <div>possibility</div>
+  )
+}
+
+export default Possibility
